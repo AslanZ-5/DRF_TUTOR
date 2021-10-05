d5067f4cb768bb982b5b40b40dcdd5ef74d08bde
Aslan <asl.zurabov@gmail.com>
2021-10-05 10:37:18 +0300

    first commit DRF

---
+++ snippets/api/viewset.py
@@ -0,0 +1,22 @@
+from snippets.models import Snippet
+from .serializers import SnippetSerializer
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
+
+# class SnippetViewSet(viewsets.ViewSet):
+#     def list(self,request):
+#         queryset = Snippet.objects.all()
+#         serializer = SnippetSerializer(queryset,many=True)
+#         return Response(serializer.data)
+
+class SnippetViewSet(viewsets.ModelViewSet):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    @action(methods=['get'], detail=False)
+    def newest(self, request):
+        newest = self.get_queryset().order_by('created').last()
+        serializer = self.get_serializer_class()(newest)
+        return Response(serializer.data)
